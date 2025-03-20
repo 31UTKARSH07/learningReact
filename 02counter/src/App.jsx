28919744b10d62985cc56877e82a45f35f019736
Utkarsh <3107utkarshpathak@gmail.com>
2025-03-20 16:02:12 +0530

    more code

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -4,24 +4,26 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  let counter = 5;
-
+  let [counter, setCounter] = useState(15) //[variable, function which controls variable]
+  //let counter = 5;
   const addValue = () => {
-    console.log("clicked", Math.random());
-    counter = counter + 1;
+    //console.log("clicked", Math.random());
+    //counter = counter + 1;
+   setCounter(counter>=20 ? 20 : counter+1)
   }
-
+  const removeValue = () => {
+    setCounter(counter<=0 ? 0 : counter-1)
+  }
   return (
     <>
-    <h1>Chai aur react</h1>
-    <h2>Counter value: 5</h2>
+      <h1>Chai aur react</h1>
+      <h2>Counter value: {counter}</h2>
 
-    <button onClick={addValue}>Add {addValue}</button>
-    <br></br>
-    <button>remove {addValue}</button>
-    <p>footer: {counter}</p>
+      <button onClick={addValue}>Add {counter}</button>
+      <br></br>
+      <button onClick={removeValue}>remove {counter}</button>
+      <p>footer: {counter}</p>
     </>
   )
 }
-
 export default App
